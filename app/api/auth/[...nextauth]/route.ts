fec64d917bbee0027068794930c9ee5b7c9bd13f
vc-cyber-coder <varunchaurasia203@gmail.com>
2025-04-27 15:42:53 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
-import { compare } from "bcrypt"
+import { compare } from "bcryptjs"
 import clientPromise from "@/lib/mongodb"
 
 export const authOptions = {
